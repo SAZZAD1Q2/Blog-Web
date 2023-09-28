c81fbe6a3747bfa932995be4a6bec4a52736c9fe
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-28 23:09:23 +0600

    form add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,36 @@
 # rubocop:disable all
 class PostsController < ApplicationController
-  def index; end
-
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
 
-  def show; end
-
   def show
     @post = Post.includes(:comments).find(params[:id])
     @user = User.find(params[:user_id])
+    @likes = @post.likes.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_post_url(@user, @post)
+
+    else
+      flash.now[:error] = 'Oops, something went wrong'
+      redirect_to new_post_url
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
