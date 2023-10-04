6adb2095212e224ad2299cb382382fdf9706de52
Theodoraldo <theodoraldo@gmail.com>
2023-10-04 06:56:21 +0000

    Modify likes and users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   attribute :posts_counter, :integer, default: 0
   has_many :posts, foreign_key: 'author_id'
-  has_many :likes
-  has_many :comments
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
