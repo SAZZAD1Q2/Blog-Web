ef0e64ddd82effaa95bf911eee4fce3248643154
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-06 14:19:29 +0600

    add devise

---
+++ app/controllers/unlocks_controller.rb
@@ -0,0 +1,13 @@
+class UnlocksController < ApplicationController
+    # GET /unlock
+    def new
+      # Render the unlock instructions resend form (view).
+    end
+  
+    # POST /users/unlock
+    def create
+      # Logic to resend unlock instructions
+      # (keep the same logic as you have in your previous code)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
-  # Remove or comment out this line
-  # devise_for :users
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
-  post '/users/:user_id/posts', to: 'posts#create', as: 'create_post'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  resources :users, only: [] do
-    resources :posts, only: [] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:new, :create]
-    end
+  devise_for :users, controllers: { unlocks: 'unlocks' }
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Root route, you can change this to your desired root path.
+  # By default, it's set to the "users#index" action.
+  root "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
+
+  resources :posts, only: [:new, :create, :destroy]
+  resources :comments, only: [:destroy]
+
+  post '/posts/:post_id/comments', to: 'comments#create', as: 'post_comments'
+  post '/posts/:post_id/likes', to: 'likes#create', as: 'post_likes'
+
+  # Add a custom route for unlocking accounts
+  get '/unlock', to: 'unlocks#new', as: 'new_unlock'
+  post '/unlock', to: 'unlocks#create', as: 'unlock'
 end
